e61798f2d20bd62a6d63145b4e609416a19afcfc
Shiri Mund <shiribogo@gmail.com>
2018-04-15 16:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens= ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name,array) {
+  [name,...array]
+}
+
+function prependKitten(name, array) {
+  [array, name...]
+}
+
+function removeLastKitten(name) {
+  kittens.splice(0,1)
+}
+
+function removeFirstKitten(name) {
+  kittens.splice(0,1)
+}
